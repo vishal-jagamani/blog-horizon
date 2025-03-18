67d6f30edde0f13b3108ed595d5973d4dc1165ae
Vishal Jagamani <vishaljagamani20@gmail.com>
2025-03-18 18:54:12 +0530

    Basic setup

---
+++ Frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="flex h-screen flex-col items-center justify-center space-y-4">
+            <h1 className="text-4xl font-bold">Blog Horizon</h1>
+            <p className="text-lg">Coming soon...</p>
+        </div>
+    );
+};
+
+export default Home;
