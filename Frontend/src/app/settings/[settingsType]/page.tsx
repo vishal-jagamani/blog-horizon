14815236139ba451d4f9e30ea686dad1a71bd6c1
Vishal Jagamani <vishaljagamani20@gmail.com>
2025-03-30 22:45:17 +0530

    Update page.tsx

--- Frontend/src/app/settings/[settingsType]/page.tsx
+++ Frontend/src/app/settings/[settingsType]/page.tsx
@@ -1,6 +1,6 @@
 import Settings from '@/components/settings/Settings';
 
-const Page = async ({ params }: { params: { settingsType: string } }) => {
+const Page = async ({ params }: { params: Promise<{ settingsType: string }> }) => {
     const { settingsType } = await params;
     return <Settings settingsType={settingsType} />;
 };
