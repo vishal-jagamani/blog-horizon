df9ea81c87e4bb849174dbfb0e6911ac7e4dd901
Vishal Jagamani <vishaljagamani20@gmail.com>
2025-06-27 23:24:29 +0530

    updated auth0 client

--- Frontend/src/lib/auth0.ts
+++ Frontend/src/lib/auth0.ts
@@ -19,6 +19,6 @@ export const auth0 = new Auth0Client({
         // In v4, the AUTH0_SCOPE and AUTH0_AUDIENCE environment variables for API authorized applications are no longer automatically picked up by the SDK.
         // Instead, we need to provide the values explicitly.
         scope: process.env.AUTH0_SCOPE,
-        audience: process.env.AUTH0_AUDIENCE,
+        // audience: process.env.AUTH0_AUDIENCE,
     },
 });
